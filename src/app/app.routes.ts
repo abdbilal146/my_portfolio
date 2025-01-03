845cd1c32dd2f033c99be498e196a82b182ef813
Mancer Abd-ElFetah <abdelfetahmancer@gmail.com>
2025-01-03 22:57:08 +0100

    routers updated , contact-page & skills page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SkillsPageComponent } from './components/skills-page/skills-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomePageComponent},
+    {path: 'skills', component: SkillsPageComponent },
+    {path:'contact-me', component: ContactPageComponent}
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+    constructor(private router: Router){}
+
+    goToSkillsPage():void{
+      this.router.navigate(['skills'])
+    }
+
+    goToHomePage():void{
+      this.router.navigate([''])
+    }
+
+    goToContactPage(){
+      this.router.navigate(['contact-me'])
+    }
 }
