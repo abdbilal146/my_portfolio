791b9215eb69e93c5e503468e818462fb883641a
abdbilal146 <abdelfetahmancer@gmail.com>
2025-04-11 17:12:13 +0200

    translate logic added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    (TranslateModule.forRoot({
+      defaultLanguage: 'fr', 
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }).providers as [])
+  ]
 };
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
-    constructor(private router: Router){}
+    public languageMenuVisibility: boolean = false;
+    public translate = inject(TranslateService);
+   
+
+    constructor(private router: Router){
+      this.translate.setDefaultLang('fr');
+      this.translate.use('fr')
+    }
+
+    @HostListener('document:click')
+    onDocumentClick() {
+    // Close the element if it's open and the click reaches the document
+      if (this.languageMenuVisibility) {
+        this.languageMenuVisibility = false;
+      }
+    }
+    
+
+    switchLanguage(lang:string):void{
+      this.translate.use(lang)
+    }
+
+    openLanguageMenu():void{
+      this.languageMenuVisibility = true;
+    }
+    closeLanguageMenu():void{
+      this.languageMenuVisibility = false;
+    }
 
     goToExperiencePage():void{
       this.router.navigate(['experience'])
